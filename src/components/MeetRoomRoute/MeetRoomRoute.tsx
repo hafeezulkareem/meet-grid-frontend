b6ef7b972320ef282b1011f214d87bc2a25d7018
Hafeezul Kareem <hafeezulkareem@gmail.com>
2023-12-23 11:33:52 +0530

    Setup socket io

--- src/components/MeetRoomRoute/MeetRoomRoute.tsx
+++ src/components/MeetRoomRoute/MeetRoomRoute.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 import Footer from "./Footer/Footer";
+import { Socket, io } from "socket.io-client";
 
 const MeetRoomRoute = () => {
   const { roomId } = useParams();
+
+  const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    if (socketRef.current) return;
+
+    socketRef.current = io("http://localhost:4000/");
+    const socket = socketRef.current;
+
+    socket?.on("connect", () => {
+      const user = { name: "Hafeez", socketId: socket?.id };
+      socket?.emit("addUser", user);
+    });
+  }, []);
 
   return (
     <Box sx={{ width: "100vw", height: "100vh", backgroundColor: "black" }}>
